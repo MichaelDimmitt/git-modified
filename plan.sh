1440757c7fd29d08ba071c10580857e34f066e7a
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-12-21 15:14:02 -0500

    Update plan.sh

--- plan.sh
+++ plan.sh
@@ -3,3 +3,10 @@ run
 git status -s; 
 
 on the list
+
+
+git caching ... next to git config.
+
+git clone maps an additional command to add to cache.
+
+rm maps an additional command to remove from cache.
